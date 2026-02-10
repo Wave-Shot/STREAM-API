fdeb3258581ff0aaa7f6b98a9b3cfd6a45af4410
Wave-Shot <vsh.2509@gmail.com>
2026-02-10 12:03:30 +0530

    [Srihari] : UC3

--- src/StreamOperations.java
+++ src/StreamOperations.java
@@ -4,8 +4,9 @@ import java.util.stream.*;
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2,3,4,5);
-        list.stream()
+        List<Integer> result = list.stream()
                 .map(n -> n * 2)
-                .forEach(System.out::println);
+                .collect(Collectors.toList());
+        System.out.println(result);
     }
 }
