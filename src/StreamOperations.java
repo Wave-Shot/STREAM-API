dd85bed07a13a17ffe10570fe51ad188b233aba9
Wave-Shot <vsh.2509@gmail.com>
2026-02-10 12:06:05 +0530

    [Srihari] : UC8

--- src/StreamOperations.java
+++ src/StreamOperations.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5);
-        int sum = list.stream().mapToInt(Integer::intValue).sum();
-        double avg = list.stream().mapToInt(Integer::intValue).average().getAsDouble();
-        System.out.println("Sum: " + sum);
-        System.out.println("Average: " + avg);
+        List<Integer> list = Arrays.asList(2,4,6,7);
+        boolean allEven = list.stream().allMatch(n -> n % 2 == 0);
+        boolean anyEven = list.stream().anyMatch(n -> n % 2 == 0);
+        System.out.println("All Even: " + allEven);
+        System.out.println("Any Even: " + anyEven);
     }
 }
