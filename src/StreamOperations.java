5514a7f5eb984c6faa991d4f7370b991fd194cfb
Wave-Shot <vsh.2509@gmail.com>
2026-02-10 12:05:03 +0530

    [Srihari] : UC6

--- src/StreamOperations.java
+++ src/StreamOperations.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,3,5,6,8);
-        Optional<Integer> firstEven = list.stream()
-                .filter(n -> n % 2 == 0)
-                .findFirst();
-        System.out.println(firstEven.get());
+        List<Integer> list = Arrays.asList(2,4,6,8,10);
+        int min = list.stream().min(Integer::compare).get();
+        int max = list.stream().max(Integer::compare).get();
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
     }
 }
